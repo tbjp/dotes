0c86b7d0c03477501187e8464b82b93f9fe65bc8
JarodMizgalski <jarodmiz2018@gmail.com>
2024-11-27 10:50:59 +0900

    fixed suggested topics array

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -10,6 +10,7 @@ class PodcastsController < ApplicationController
   def new
     @podcast = Podcast.new
     #pass instance of user language
+    @user_language = current_user.selected_user_language
   end
 
   def create
