c3812e30428f34f6db70def2fad3874ae3372da1
Toby Palethorpe <tobypalethorpe@gmail.com>
2024-11-28 09:52:44 +0900

    implemented flow for adding a podcast, then disabled API for now with dummy returns, removed suggested topics from form temporarily

--- app/services/generate_audio.rb
+++ app/services/generate_audio.rb
@@ -4,24 +4,25 @@ require 'json'
 
 class GenerateAudio
 
-  def self.call
+  def self.call(transcript)
+    return '/audios/placeholder.mp3' # skip API call for now
     key = ENV.fetch("AZURE_API_KEY")
     region = 'japaneast'
 
     uri = URI("https://#{region}.tts.speech.microsoft.com/cognitiveservices/v1")
 
     # SSML body for the TTS
-    # ssml = transcript
-    ssml = '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-GB">
-    <voice name="en-GB-OllieMultilingualNeural">
-        <lang xml:lang="es-MX">¡Esperamos trabajar con usted!</lang>
-        <lang xml:lang="en-GB">We look forward to working with you!</lang>
-        <lang xml:lang="ja-JP">私たちはあなたと一緒に仕事をするのを楽しみにしています！</lang>
-        <lang xml:lang="en-GB">The word,</lang>
-        <lang xml:lang="ja-JP">鴨,</lang>
-        <lang xml:lang="en-GB">means duck.</lang>
-    </voice>
-</speak>'
+    ssml = transcript
+#     ssml = '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-GB">
+#     <voice name="en-GB-OllieMultilingualNeural">
+#         <lang xml:lang="es-MX">¡Esperamos trabajar con usted!</lang>
+#         <lang xml:lang="en-GB">We look forward to working with you!</lang>
+#         <lang xml:lang="ja-JP">私たちはあなたと一緒に仕事をするのを楽しみにしています！</lang>
+#         <lang xml:lang="en-GB">The word,</lang>
+#         <lang xml:lang="ja-JP">鴨,</lang>
+#         <lang xml:lang="en-GB">means duck.</lang>
+#     </voice>
+# </speak>'
 
     # Build the HTTP request headers
     headers = {
@@ -37,7 +38,7 @@ class GenerateAudio
     response = Net::HTTP.post(uri, ssml, headers)
 
     # Save the audio file
-    File.binwrite("output5.mp3", response.body)
+    File.binwrite("output7.mp3", response.body)
     puts "Audio saved as 'output.mp3'"
     return response.body
   end
